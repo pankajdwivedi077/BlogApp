0d6826be9631d509d07412b5b36fe414a336b20d
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2024-12-24 21:20:42 +0530

    part 3

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,20 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
+import Button from "./Button";
+import RTE from "./RTE";
+import Select from "./Select";
 
 export {
     Header,
     Footer,
     Container,
     Logo, 
-    LogoutBtn
+    LogoutBtn,
+    Input,
+    Button,
+    Select,
+    RTE,
+    
 }
