40f6c350607c8babe2f44a22d5703b26763c9784
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2024-12-26 13:49:30 +0530

    env fix

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 import { Controller } from 'react-hook-form';
+import conf from "../conf/conf"
 
 export default function RTE({ name, control, label, defaultValue = "" }) {
+  
   return (
     <div className='w-full'>
       {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
@@ -11,7 +13,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
-            apiKey="h13nl14ruv9w7x8v429zmm7jh3rfdp45gzawuf7uarloaydw" // Add your API key here
+            apiKey={conf.apikeyrte} // Add your API key here
             initialValue={defaultValue}
             init={{
               height: 500,
